7fd0839029f46746425c57496af6b8780837e90b
HarrisYu5 <hy002421@bu.edu>
2023-11-30 15:42:54 -0500

    Added second activity

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MainPage.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MainPage.kt
@@ -3,10 +3,9 @@ package com.bignerdranch.android.bostonufacebook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
-        //github test
     }
 }
