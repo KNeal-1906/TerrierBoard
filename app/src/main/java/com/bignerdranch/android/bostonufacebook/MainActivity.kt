6e7cb53472b8998d4f9476ef1d886e1251c6f521
HarrisYu5 <hy002421@bu.edu>
2023-11-30 16:13:59 -0500

    Added main page and navigation from login page to main page. (Main is not displaying properly)

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MainActivity.kt
@@ -2,11 +2,17 @@ package com.bignerdranch.android.bostonufacebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_page)
-        //github test
+        setContentView(R.layout.login)
+        val loginButton = findViewById<Button>(R.id.guest_btn)
+        loginButton.setOnClickListener {
+            // Switch to R.layout.page when the button is clicked
+            setContentView(R.layout.activity_main_page)
+    }
+
     }
 }
