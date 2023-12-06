7155a83e68f1e8d7ce2713e04e2cac626549cd63
Kenise Neal <kenneal@bu.edu>
2023-12-06 02:17:25 -0500

    Adding Layout of Home Page, and added a few components for Google Sign In

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        val loginButton = findViewById<Button>(R.id.guest_btn)
-        loginButton.setOnClickListener {
+        val guestButton = findViewById<Button>(R.id.guest_btn)
+        guestButton.setOnClickListener {
             // Switch to R.layout.page when the button is clicked
-            setContentView(R.layout.activity_main_page)
+            setContentView(R.layout.activity_main)
     }
 
     }
