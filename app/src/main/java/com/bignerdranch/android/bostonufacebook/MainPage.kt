5a735cb08abb655e7aeb60e3c575bf496851b6b5
HarrisYu5 <hy002421@bu.edu>
2023-12-14 09:25:19 -0500

    Added google signin function

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MainPage.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MainPage.kt
@@ -1,11 +1,50 @@
 package com.bignerdranch.android.bostonufacebook
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainPage : AppCompatActivity() {
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val sign_out_button = findViewById<Button>(R.id.sign_out_btn)
+        sign_out_button.setOnClickListener {
+            signOutAndStartSignInActivity()
+        }
+
+
+    }
+    private fun signOutAndStartSignInActivity(){
+        mAuth.signOut()
+
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+            // Optional: Update UI or show a message to the user
+            val intent = Intent(this@MainPage, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
