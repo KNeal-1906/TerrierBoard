f78da624023d0804af293abf42826cb4b36bca9b
HarrisYu5 <hy002421@bu.edu>
2023-12-21 01:39:41 -0500

    Added binding for the makepost page

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MakePost.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MakePost.kt
@@ -2,11 +2,17 @@ package com.bignerdranch.android.bostonufacebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.bostonufacebook.databinding.ActivityMainBinding
+import com.bignerdranch.android.bostonufacebook.databinding.ActivityMakePostBinding
+import com.google.firebase.database.DatabaseReference
 
 class MakePost : AppCompatActivity() {
+    private lateinit var binding: ActivityMakePostBinding
+        private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMakePostBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_make_post)
-        //hi
+
     }
 }
