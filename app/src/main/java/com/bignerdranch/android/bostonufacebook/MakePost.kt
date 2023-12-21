a705154e8be116dab1ad41f35313816c613f8f26
HarrisYu5 <hy002421@bu.edu>
2023-12-21 01:23:31 -0500

    Changed the trade button to the Go to post page, added a new activity called MakePost

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/MakePost.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/MakePost.kt
@@ -7,5 +7,6 @@ class MakePost : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_post)
+        //hi
     }
 }
