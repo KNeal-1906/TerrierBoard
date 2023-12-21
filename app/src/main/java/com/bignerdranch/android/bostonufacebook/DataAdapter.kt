3c910c36a3c707b14cbd74cc2ad68eda9322a62b
HarrisYu5 <hy002421@bu.edu>
2023-12-21 17:51:20 -0500

    Added some comment, made the recyclerview a little nicer to look at

--- app/src/main/java/com/bignerdranch/android/bostonufacebook/DataAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/bostonufacebook/DataAdapter.kt
@@ -10,8 +10,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.bostonufacebook.Models.Data
 
-
+//It takes an ArrayList of Data objects as a parameter, used to populate the RecyclerView.
 class DataAdapter(private val dataList: ArrayList<Data>):RecyclerView.Adapter<DataAdapter.DataHolder>() {
+    //Responsible for holding references to the views in each item of the RecyclerView, like userName, comment, and imageView.
     class DataHolder(dataView:View):RecyclerView.ViewHolder(dataView){
         val userName:TextView=dataView.findViewById(R.id.UserName)
         val comment:TextView=dataView.findViewById(R.id.Comment)
@@ -21,6 +22,7 @@ class DataAdapter(private val dataList: ArrayList<Data>):RecyclerView.Adapter<Da
     }
 
     override fun onCreateViewHolder(
+        //Responsible for inflating the item layout (R.layout.data_item) and returning a new DataHolder instance.
         parent: ViewGroup,
         viewType: Int
     ): DataHolder {
@@ -29,6 +31,8 @@ class DataAdapter(private val dataList: ArrayList<Data>):RecyclerView.Adapter<Da
     }
 
     override fun onBindViewHolder(
+        //It sets the text for userName and comment views and decodes a Base64 encoded image string
+        // to a Bitmap, which is then set on an ImageView
         holder: DataHolder,
         position: Int
     ) {
